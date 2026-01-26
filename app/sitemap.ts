547e363773a7ef198c9215475ff8f6a7ee5f1190
MCS4885A <scott.michelon@etu.iut-tlse3.fr>
2026-01-26 12:15:36 +0100

    chore(seo): add sitemap for search engines

---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import {MetadataRoute} from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseURL = 'https://portfolio-michelon-scott.vercel.app';
+    return [
+        {
+            url: '${baseURL}/',
+            lastModified: new Date()
+        },
+        {
+            url: '${baseURL}/competences/',
+            lastModified: new Date()
+        },
+        {
+            url: '${baseURL}/projets/',
+            lastModified: new Date()
+        },
+        {
+            url: '${baseURL}/cv',
+            lastModified: new Date()
+        },
+        {
+            url: '${baseURL}/contact/',
+            lastModified: new Date()
+        }
+    ];
+}
